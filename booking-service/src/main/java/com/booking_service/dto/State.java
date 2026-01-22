64467c5417d9bc662c1cefafc8c8785fc5d153f7
poojapoojithahm <poojapoojithahm@gmail.com>
2026-01-22 14:27:25 +0530

    Create State.java

---
+++ booking-service/src/main/java/com/booking_service/dto/State.java
@@ -0,0 +1,27 @@
+package com.booking_service.dto;
+
+
+public class State {
+    private Long id;
+
+    private String name;
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+}

01836bc859e6135621fb00f9da8f9e6007ca069b
poojapoojithahm <poojapoojithahm@gmail.com>
2026-01-22 16:05:25 +0530

    Create City.java

---
+++ doctor-service/src/main/java/com/doctor_Service/entity/City.java
@@ -0,0 +1,32 @@
+package com.doctor_service.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "city")
+public class City {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+}

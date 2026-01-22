2cc05d02001de1916430fab67b7a164bde1fc7b7
poojapoojithahm <poojapoojithahm@gmail.com>
2026-01-22 16:07:02 +0530

    Create DoctorAppointmentSchedule.java

---
+++ doctor-service/src/main/java/com/doctor_Service/entity/DoctorAppointmentSchedule.java
@@ -0,0 +1,62 @@
+package com.doctor_service.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Table(name = "doctor_appointment_schedule")
+public class DoctorAppointmentSchedule {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "doctor_id")
+    @JsonBackReference
+    private Doctor doctor;
+
+    @OneToMany(mappedBy = "doctorAppointmentSchedule", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JsonManagedReference
+    private List<TimeSlots> timeSlots;
+
+
+    @Column(name = "date")
+    private LocalDate date;
+
+    public Doctor getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(Doctor doctor) {
+        this.doctor = doctor;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<TimeSlots> getTimeSlots() {
+        return timeSlots;
+    }
+
+    public void setTimeSlots(List<TimeSlots> timeSlots) {
+        this.timeSlots = timeSlots;
+    }
+}
